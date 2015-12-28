3bfa412bc29ea233cbc9077a119c4b07cd49923e
Jin Choi <jinchoi721@gmail.com>
2015-12-28 11:19:29 -0600

    first commit

---
+++ get_observation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#this script get observation after run split_libraries.py
+#usage: python get_observation.py
+#then, the program read all .summary files in the folder and make observation.txt
+
+
+
+#import os
+#for filename in os.listdir(os.getcwd()):
+#    print filename
+import glob
+fwrite = open('observation.txt','w')
+for filename in glob.glob('*.summary'):
+    print filename
+    fo = open(filename,'r')
+    line = fo.readline()
+    line = fo.readline().strip()
+    print line
+    fwrite.write(filename+'\t'+line+'\n')
+    fo.close()

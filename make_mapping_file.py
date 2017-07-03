c37ef2ad7591f93c671efa6c2faa8484df2c460c
Jin Choi <jinchoi721@gmail.com>
2017-07-03 14:41:24 -0500

    update

---
+++ make_mapping_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""
+this script make mapping file from the files in the directory
+usage: python make_mapping_file.py dir_name > mapping.file.txt
+example: python make_mapping_file.py merged > mapping.file.txt
+"""
+
+import os
+import sys
+from os import listdir
+from os.path import isfile, join
+path = sys.argv[1]
+files = [f for f in listdir(path) if isfile(join(path,f))]
+
+print "#SampleID\tInputFileName"
+for file in files:
+    sample_id = file.replace('.fastq','')
+    print sample_id + '\t' + file
+

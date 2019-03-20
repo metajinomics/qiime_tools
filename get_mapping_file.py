ae80c57c109410025382f852b43d65c8c87c6c51
Jin Choi <jinchoi721@gmail.com>
2019-03-19 21:06:45 -0500

    udpate

---
+++ get_mapping_file.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/python 
+"""
+This script do ...
+usage:
+python filename.py arg1...
+"""
+
+import sys
+
+def main():
+
+    samplelist = {}
+    for file in sys.argv[1:]:
+        if "_L001_" in file:
+            spl = file.split("_L001_")
+        else:
+            print "check file name"
+
+
+
+if __name__ == '__main__':
+    main()

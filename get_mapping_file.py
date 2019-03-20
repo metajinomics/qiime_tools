8be05dea748a603ab57d6ae74794026dba0958c4
Jin Choi <jinchoi721@gmail.com>
2019-03-19 21:10:12 -0500

    udpate

--- get_mapping_file.py
+++ get_mapping_file.py
@@ -14,7 +14,7 @@ def main():
     for file in sys.argv[1:]:
         if "_L001_" in file:
             spl = file.split("_L001_")
-            samplelist[spl0]] = samplelist.get(spl[0],[]).append(spl[1])
+            samplelist[spl[0]] = samplelist.get(spl[0],[]).append(spl[1])
         else:
             print ("check file name")
 
